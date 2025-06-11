da751cf6a7a6322c5622a8e3c8a7b3bfdf7bdf42
bharti-kajal <kajalbharti990@gmail.com>
2025-06-11 21:07:37 +0530

    seed

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
-// import './seed.js';
 import express from 'express';
 import cors from 'cors';
 import router from './src/config/router.js';
 import { connectUsingMongoose } from './src/config/dbConfig.js';
+import seedData from './seed.js'; 
 
 const server = express();
 const port = 3200;
 
 const corsOptions = {
-    origin: "https://dsa-page-sheet.netlify.app",
-    methods: "GET,POST",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
+  origin: "https://dsa-page-sheet.netlify.app",
+  methods: "GET,POST",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true
 };
 server.use(cors(corsOptions));
-
-// Middlewares
 server.use(express.json());
-
-// Routes
 server.use('/api', router);
 
-
-// 404 Error Handler
-server.use((req, res, next) => {
-  res.status(404).json({
-    status: false,
-    message: 'The requested endpoint does not exist'
-  });
+server.use((req, res) => {
+  res.status(404).json({ status: false, message: 'The requested endpoint does not exist' });
 });
 
 server.listen(port, async () => {
-  await connectUsingMongoose();
+  await connectUsingMongoose();  
+  await seedData();           
   console.log(`Server running on port ${port}`);
 });
